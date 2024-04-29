c9bce7b2fb6412413e49c521224814530e1cf2d7
Bruno Bernardo <bigsby@live.com>
2024-04-29 16:36:58 +0100

    draw borders

--- code/cs/Display.cs
+++ code/cs/Display.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Numerics;
 using static System.Console;
 
@@ -14,14 +15,32 @@ internal static class Display
 
     static int[] COLOURS = [196, 21, 28, 226, 208, 51, 206, 88, 90];
 
-    public static string GetColourDot(int colour)
+    public static string GetColourDot(int colour, bool highlight = false)
         => colour == -1 ?
         "\u25E6"
         :
-        $"\x1b[38;5;{COLOURS[colour]}m\u25CF\x1b[0m";
+        $"\x1b[38;5;{COLOURS[colour]}m{(highlight ? "\x1b[48;5;47m" : "")}\u25CF\x1b[0m";
     
+    private static string GetColourRectangle(int colour, bool vertical)
+        => colour == -1 ?
+        " "
+        :
+        $"\x1b[38;5;{COLOURS[colour]}m{(vertical ? '\u25AE' : '\u25AC')}\x1b[0m";
+
     public static void Error(string message)
         => Console.Error.WriteLine($"\x1b[38;5;196m{message}\x1b[0m");
+
+    public static void Clear()
+        => Console.Clear();
+
+    public static void GoToTop()
+        => Write("\x1b[0;0H");
+
+    public static void Print(string text)
+        => WriteLine(text);
+
+    public static void Key()
+        => ReadLine();
 
     private static int IndexOf<T>(this IEnumerable<T> source, Func<T, bool> filter)
     {
@@ -36,54 +55,100 @@ internal static class Display
         return -1;
     }
 
-    public static void Write(string text)
-        => WriteLine(text);
-
-    public static void Key()
-        => ReadLine();
-
-    public static int GetColour(this Puzzle puzzle, Complex position)
+    private static int GetColour(this Puzzle puzzle, Complex position)
         => puzzle.Colours.IndexOf(ends => ends.Item1 == position || ends.Item2 == position);
-
-    public static void Print(this Puzzle puzzle, IDictionary<Complex, int>? solution = default, bool clear = false, bool step = false)
+    
+    private static IReadOnlyDictionary<Walls, char> BORDERS = new Dictionary<Walls, char>
     {
-        if (clear)
-            Clear();
-        WriteLine($"{puzzle.Name} {puzzle.SubTitle} {puzzle.MaxX} {puzzle.MaxY}");
-        for (double y = 0; y < puzzle.MaxY * 2 + 3; y++)
+        { Walls.NONE, ' ' },
+        { Walls.LEFT | Walls.DOWN, '\u2510' },
+        { Walls.LEFT | Walls.UP, '\u2518' },
+        { Walls.LEFT | Walls.UP | Walls.DOWN, '\u2524' },
+        { Walls.LEFT | Walls.RIGHT | Walls.UP | Walls.DOWN, '\u253C' },
+        { Walls.LEFT | Walls.RIGHT, '\u2500' },
+        { Walls.LEFT | Walls.RIGHT | Walls.DOWN, '\u252C' },
+        { Walls.RIGHT | Walls.DOWN, '\u250C' },
+        { Walls.RIGHT | Walls.UP, '\u2514' },
+        { Walls.RIGHT | Walls.UP | Walls.DOWN, '\u251C' },
+        { Walls.UP | Walls.DOWN, '\u2502' },
+        { Walls.RIGHT, '\u2576' },
+        { Walls.LEFT, '\u2574' },
+        { Walls.UP, '\u2575' },
+        { Walls.DOWN, '\u2577' },
+    };
+
+    private static bool HasWall(this Puzzle puzzle, Complex position, Walls wall)
+        => puzzle.Positions.TryGetValue(position, out var value) && value.HasFlag(wall);
+    
+    private static bool HasPosition(this Puzzle puzzle, Complex position)
+        => puzzle.Positions.ContainsKey(position);
+
+    public static void Print(this Puzzle puzzle, IDictionary<Complex, int>? solution = default, Complex? move = default)
+    {
+        Print($"{puzzle.Name} {puzzle.SubTitle} {puzzle.MaxX} {puzzle.MaxY}");
+        for (double y = 0; y < puzzle.MaxY * 2 + 1; y++)
         {
-            for (double x = 0; x < puzzle.MaxX * 2 + 3; x++)
+            for (double x = 0; x < puzzle.MaxX * 2 + 1; x++)
             {
                 var c = " ";
+                var padding = " ";
                 var position = new Complex(
                     (x - 1) / 2,
                     (y - 1) / 2);
-                if (x % 2 == 1 && y % 2 == 1) // position
+                if (x % 2 == 1 && y % 2 == 1 && puzzle.Positions.ContainsKey(position)) // position
                 {
                     if (solution?.TryGetValue(position, out var colour) ?? false)
-                        c = GetColourDot(colour);
+                        c = GetColourDot(colour, position == move);
                     else
                         c = GetColourDot(GetColour(puzzle, position));
                 }
                 else // wall
                 {
                     if (x % 2 == 0 && y % 2 == 0)
-                        c = " "; if (y % 2 == 1)
+                    {
+                        var hasLeft = puzzle.HasWall(position + new Complex(-.5, .5), Walls.UP) || puzzle.HasWall(position + new Complex(-.5, -.5), Walls.DOWN);
+                        var hasRight = puzzle.HasWall(position + new Complex(.5, .5), Walls.UP) || puzzle.HasWall(position + new Complex(.5, -.5), Walls.DOWN);
+                        var hasUp = puzzle.HasWall(position + new Complex(-.5, -.5), Walls.RIGHT) || puzzle.HasWall(position + new Complex(.5, -.5), Walls.LEFT);
+                        var hasDown = puzzle.HasWall(position + new Complex(-.5, .5), Walls.RIGHT) || puzzle.HasWall(position + new Complex(.5, .5), Walls.LEFT);
+                        var lines = 
+                            (hasLeft ? 1 : 0) * (int)Walls.LEFT + 
+                            (hasRight ? 1 : 0) * (int)Walls.RIGHT + 
+                            (hasUp ? 1 : 0) * (int)Walls.UP + 
+                            (hasDown ? 1 : 0) * (int)Walls.DOWN;
+                        c = $"{BORDERS[(Walls)lines]}";
+                        if (hasLeft && (puzzle.HasPosition(position + new Complex(.5, -.5)) ^ puzzle.HasPosition(position + new Complex(.5, .5))))
+                            padding = "\u2500";
+                        if (hasRight && (puzzle.HasPosition(position + new Complex(.5, -.5)) || puzzle.HasPosition(position + new Complex(.5, .5))))
+                            padding = "\u2500";
+                    }
+                    if (y % 2 == 1)
                     {
                         foreach (var (direction, wall) in VERTICAL)
                             if (puzzle.GetNeighbour(position, direction).HasFlag(wall))
-                                c = "\u2503";
+                                c = "\u2502";
                     }
                     else if (x % 2 == 1)
                         foreach (var (direction, wall) in HORIZONTAL)
                             if (puzzle.GetNeighbour(position, direction).HasFlag(wall))
-                                c = "\u2501";
+                            {
+                                c = "\u2500";
+                                padding = "\u2500";
+                            }
+                    if (c == " " && null != solution)
+                    {
+                        if (solution.TryGetValue(puzzle.NormalizePosition(position + new Complex(-.5, 0)), out var left)
+                            && solution.TryGetValue(puzzle.NormalizePosition(position + new Complex(.5, 0)), out var right)
+                            && left == right)
+                            c = GetColourRectangle(left, false);
+                        if (solution.TryGetValue(puzzle.NormalizePosition(position + new Complex(0, -.5)), out var up)
+                            && solution.TryGetValue(puzzle.NormalizePosition(position + new Complex(0, .5)), out var down)
+                            && up == down)
+                            c = GetColourRectangle(up, true);
+                    }
                 }
-                System.Console.Write($"{c} ");
+                Write($"{c}{padding}");
             }
             WriteLine();
         }
-        if (step)
-            ReadLine();
     }
 }
